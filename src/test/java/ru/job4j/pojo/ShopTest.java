3aa6bca004cb8f47d267253bde183af11cbe7075
zenit1925 <kostia-85-85@yandex.ru>
2022-05-25 16:54:41 +0300

    3.1. Массив с пустыми ячейками - Ошибка сборки

--- src/test/java/ru/job4j/pojo/ShopTest.java
+++ src/test/java/ru/job4j/pojo/ShopTest.java
@@ -5,7 +5,6 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class ShopTest {
-
     @Test
     public void whenLastNull() {
         Product[] products = new Product[5];
