2207b8c1aa1110d686a9370fb12065f216e24a90
zenit1925 <kostia-85-85@yandex.ru>
2022-05-27 10:22:46 +0300

    8. Валидация методов replace, delete [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -81,18 +81,24 @@ public class Tracker {
      */
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        item.setId(id);
-        items[index] = item;
-        return true;
+        boolean result = index != -1;
+        if (result) {
+            item.setId(id);
+            items[index] = item;
+        }
+        return result;
     }
 
     /** Метод удаления заявки.
      */
     public boolean delete(int id) {
         int index = indexOf(id);
-        System.arraycopy(items, index + 1, items, index, size - index - 1);
-        items[size - 1] = null;
-        size--;
-        return true;
+        boolean result = index != -1;
+        if (result) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+        return result;
     }
 }
