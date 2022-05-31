25a0f471677f90ce1c34b4268a9ea1b90954fe31
zenit1925 <kostia-85-85@yandex.ru>
2022-05-31 14:52:27 +0300

    4.1. Разрыв зависимости StartUI от Scanner.

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,41 +1,15 @@
 package ru.job4j.tracker;
 
-import java.util.Scanner;
-
 public class StartUI {
-    /**
-     * Метод init() - инициализирует приложение и запускает выполнение различных пользовательских действий;
-     * Сделает вечный цикл while в метод init() внутри которого будем вызывать метод showMenu()
-     * int select = Integer.parseInt(scanner.nextLine()) это перменная проиницилизирована методом
-     * который отвечает за ввод с консоли, по аналогии с тем как мы делали в предыдущих заданиях.
-     * Далее в блоке if
-     * 1. Сообщить пользователю, что мы находимся в пункте добавления новой заявки в хранилище;
-     * 2. Сообщить пользователю, что он должен ввести имя для создания заявки;
-     * 3. Создать новую заявку с именем, которое мы ввели с консоли;
-     * 4. Добавить заявку в хранилище Tracker.
-     * 5. выводим сообщение пользователю с номером заявки "Добавленная заявка: " + item
-     * Следующий блок позволяет реализовать вывод всех заявок если пункт меню будет 1. Вывести все заявки, а
-     * если их нет, то сообщение что хранилище пусто
-     * Следующий блок позволяет реализовать замену заявок если пункт меню будет 2
-     * Здесь мы выводим информацию для пользователя, используем метод реплейс а так же создаем проверку на ошибку при замене заявки
-     * Следующий блок позволяет реализовать удаление заявок если пункт меню будет 3
-     * Здесь мы удаляем заявку с помощью метода делит, и выводим сообщения
-     * Меню 4
-     * Позволяет найти заявку по id, используем метод для поиска заявки по id в классе
-     * Tracker предназначен вспомогательный метод indexOf() который используется в методе findById():
-     * Меню 5
-     * Ищем заявку по названию, используем findByName
-     */
-    public void init(Scanner scanner, Tracker tracker) {
+
+    public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             showMenu();
-            System.out.print("Select: ");
-            int select = Integer.parseInt(scanner.nextLine());
+            int select = Integer.parseInt(input.askStr("Select: "));
             if (select == 0) {
                 System.out.println("=== Create a new Item ===");
-                System.out.print("Enter name: ");
-                String name = scanner.nextLine();
+                String name = input.askStr("Enter name: ");
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
@@ -51,10 +25,8 @@ public class StartUI {
                 }
             } else if (select == 2) {
                 System.out.println("=== Edit item ===");
-                System.out.print("Enter id: ");
-                int id = Integer.parseInt(scanner.nextLine());
-                System.out.print("Enter name: ");
-                String name = scanner.nextLine();
+                int id = Integer.parseInt(input.askStr("Enter id: "));
+                String name = input.askStr("Enter name: ");
                 Item item = new Item(name);
                 if (tracker.replace(id, item)) {
                     System.out.println("Заявка изменена успешно.");
@@ -63,8 +35,7 @@ public class StartUI {
                 }
             } else if (select == 3) {
                 System.out.println("=== Delete item ===");
-                System.out.print("Enter id: ");
-                int id = Integer.parseInt(scanner.nextLine());
+                int id = Integer.parseInt(input.askStr("Enter id: "));
                 if (tracker.delete(id)) {
                     System.out.println("Заявка удалена успешно.");
                 } else {
@@ -72,8 +43,7 @@ public class StartUI {
                 }
             } else if (select == 4) {
                 System.out.println("=== Find item by id ===");
-                System.out.print("Enter id: ");
-                int id = Integer.parseInt(scanner.nextLine());
+                int id = Integer.parseInt(input.askStr("Enter id: "));
                 Item item = tracker.findById(id);
                 if (item != null) {
                     System.out.println(item);
@@ -82,8 +52,7 @@ public class StartUI {
                 }
             }  else if (select == 5) {
                 System.out.println("=== Find items by name ===");
-                System.out.print("Enter name: ");
-                String name = scanner.nextLine();
+                String name = input.askStr("Enter name: ");
                 Item[] items = tracker.findByName(name);
                 if (items.length > 0) {
                     for (Item item : items) {
@@ -98,10 +67,6 @@ public class StartUI {
         }
     }
 
-    /**
-     * Метод showMenu() - выводит на экран меню доступных пользовательских действий;
-     * все пункты меню сделамем через массив строк и разобьем через цикл for
-     */
     private void showMenu() {
         String[] menu = {
                 "Add new Item", "Show all items", "Edit item",
@@ -115,17 +80,10 @@ public class StartUI {
 
     }
 
-    /**
-     * Main() просто запускает наше приложение
-     * В методе main() создается объект Scanner, который позволит получать данные с консоли от пользователя,
-     * Tracker будет служить хранилищем заявок.В следующей строке:  new StartUI().init(scanner, tracker);
-     * мы создаем объект класса StartUI и сразу на этом объекте вызываем метод init(). Поскольку дальше
-     * этот объект мы не будем переиспользовать, то мы не записываем созданный объект в переменную.
-     */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
-        new StartUI().init(scanner, tracker);
+        new StartUI().init(input, tracker);
     }
 
 }
