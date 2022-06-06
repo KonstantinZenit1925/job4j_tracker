a1b0510a2edb4ace68fe12c499d71f7862743fd7
zenit1925 <kostia-85-85@yandex.ru>
2022-06-06 12:26:46 +0300

    0.4. Пользовательские исключения. - Исправления

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -1,26 +1,5 @@
 package ru.job4j.ex;
 
 public class ElementNotFoundException extends Exception {
-    public static class FindEl {
-        public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-            int rsl = -1;
-            for (int i = 0; i < value.length; i++) {
-                if (value[i] == key) {
-                    System.out.println(i);
-                } else {
-                    System.out.println("Элемент не найден исключение");
-                }
-            }
-            return rsl;
-        }
-    }
 
-    public static void main(String[] args)  {
-        String[] seasons  = new String[] {"Winter", "Spring", "Summer"};
-        try {
-            FindEl.indexOf(seasons, "Autumn");
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
 }
