fc4d9e1b0c404110ba052f0047fb08c31e1b7f12
zenit1925 <kostia-85-85@yandex.ru>
2022-07-08 12:20:50 +0300

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,60 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    /** Поле содержит всех пользователей системы с привязанными к ним счетами: */
+    private final Map<Users, List<Account>> users = new HashMap<>();
+
+    /** Метод добавляет пользователя в систему */
+    public void addUser(Users user) {
+    users.putIfAbsent(user, new ArrayList<>());
+    }
+
+    /** Метод добавляет новый счет к пользователю */
+    public void addAccount(String passport, Account account) {
+        Users user = findByPassport(passport);
+        List<Account> accounts = users.get(user);
+        if (!accounts.contains(account)) {
+            accounts.add(account);
+        }
+
+    }
+    /** Метод ищет пользователя по номеру паспорта
+     * @return*/
+    public Users findByPassport(String passport) {
+        for (Users user : users.keySet()) {
+            if (user.getPassports().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+    /** Метод ищет счет пользователя по реквизитам */
+    public Account findByRequisite(String passport, String requisite) {
+        Users user = findByPassport(passport);
+        if (user != null) {
+            for (Account account : users.get(user)) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+    /** Метод предназначен для перечисления денег с одного счёта на другой счёт*/
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && destAccount != null && srcAccount.getBalance() >= amount) {
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            return true;
+        }
+        return false;
+    }
+}
