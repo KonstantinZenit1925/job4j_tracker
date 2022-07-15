85c873a0ca22dc30b89dd9db79d4a09e3c6e04a8
zenit1925 <kostia-85-85@yandex.ru>
2022-07-15 09:39:11 +0300

    2. Комбинированный компаратор [#10036 #320935] 15/07/2022

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -1,17 +1,11 @@
 package ru.job4j.collection;
 
-import org.junit.Assert;
 import org.junit.Test;
-import ru.job4j.collection.*;
-
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-
-import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
 public class JobTest {
     @Test
@@ -68,7 +62,7 @@ public class JobTest {
 
     @Test
     public void whenCompatorByNameAndProrityAndNameLn() {
-        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByNameLn()).thenComparing(new JobDescByPriority());
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 2),
                 new Job("Implement task", 2)
