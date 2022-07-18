8779ccd5719f65cffad76c5c27ce817de5e524b4
zenit1925 <kostia-85-85@yandex.ru>
2022-07-18 10:57:39 +0300

    2. Отсортировать департаменты Исправления 18/07/22

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int res = o2.split("/", 2)[0].compareTo(o1.split("/", 2)[0]);
+        int res = o2.split("/")[0].compareTo(o1.split("/")[0]);
         return res == 0 ? o1.compareTo(o2) : res;
     }
 }
--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -8,10 +8,7 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                if (start.length() > 0) {
-                    start += "/";
-                }
-                start += el;
+                start += "".equals(start) ? el : "/" + el;
                 tmp.add(start);
             }
         }
